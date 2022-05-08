3cc734d387c1c9bc23c291bb48775deb881efebe
Georgy Isaev <isaev.georgii@otr.ru>
2022-05-08 17:00:10 +0300

    test application context

--- developer-tools/spring-native-bitly/src/test/java/com/georgeisaev/springnativebitly/SpringNativeBitlyApplicationTests.java
+++ developer-tools/spring-native-bitly/src/test/java/com/georgeisaev/springnativebitly/SpringNativeBitlyApplicationTests.java
@@ -1,13 +1,20 @@
 package com.georgeisaev.springnativebitly;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringNativeBitlyApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(applicationContext);
     }
 
 }
